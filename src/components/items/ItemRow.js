36468d9f54e180e382e6647596ea398046a6c05c
Avidness <adavidness@gmail.com>
2018-07-15 16:39:45 -0500

    Rewrite the itemRow component to depend on props and not talk to redux directly

--- src/components/items/ItemRow.js
+++ src/components/items/ItemRow.js
@@ -1,52 +1,23 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import { deleteItem, updateItem } from '../../redux/actions/itemActions';
-import ItemForm from './ItemForm';
-
-class ItemRow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleOpen = this.handleOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.state = {
-      openForm: false
-    };
-  }
-  handleOpen(){
-    this.setState({ openForm: true });
-  }
-  handleClose(){
-    this.setState({ openForm: false });
-  }
-  render() {
-    let item = this.props.item;
-    return (
-      <div>
-        <span>{item.id} </span>
-        <span>{item.label} </span> 
-        <span>{item.description} </span> 
-        <button onClick={() => this.props.onUpdateItem(item)}>update</button>
-        <button onClick={() => this.props.onDeleteItem(item)}>delete</button>
-        {this.state.openForm 
-          ? <ItemForm 
-              item={item}
-              handleClose={this.handleClose}
-              handleSubmit={this.props.onUpdateItem} />
-          : null}
-        <button onClick={() => this.handleOpen()}>
-          Update Item
-        </button>
-        <br />
-        <br />
-      </div>
-    );
-  }
+export const ItemRow = (props) => {
+  let item = props.item;
+  return (
+    <div>
+      <span>{item.id} </span>
+      <span>{item.label} </span> 
+      <span>{item.description}</span> 
+      <button onClick={props.startEditing}>
+          edit
+      </button>
+      <button onClick={props.deleteItem}>
+          delete
+      </button>
+      <br />
+      <br />
+    </div>
+  );
 }
 
-const mapDispatchToProps = {
-  onDeleteItem: deleteItem,
-  onUpdateItem: updateItem
-}
 
-export default connect(null, mapDispatchToProps)(ItemRow);
+export default ItemRow;
