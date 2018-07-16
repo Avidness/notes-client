0723c2e66efafc6bc2151c6b4ac1adc1f1536278
Avidness <adavidness@gmail.com>
2018-07-15 23:16:24 -0500

    Remove unnecessary functions

--- src/containers/ItemContainer.js
+++ src/containers/ItemContainer.js
@@ -5,40 +5,19 @@ import ItemList from '../components/items/ItemList';
 import * as ItemActions from '../redux/actions/itemActions';
 
 class ItemContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openForm: false
-    };
-  }
   componentDidMount(){
     this.props.onFetchItems();
-  }
-  createItem = (item) => {
-    this.props.onCreateItem(item)
-  }
-  startEditing = (id) => {
-    this.props.onStartEditing(id)
-  }
-  cancelEditing = (id) => {
-    this.props.onCancelEditing(id)
-  }
-  updateItem = (item) => {
-    this.props.onUpdateItem(item)
-  }
-  deleteItem = (item) => {
-    this.props.onDeleteItem(item)
   }
   render() {
     return (
       <div className="item-page">
         <ItemList 
           items={this.props.items} 
-          createItem={this.createItem}
-          startEditing={this.startEditing}
-          cancelEditing={this.cancelEditing}
-          updateItem={this.updateItem}
-          deleteItem = {this.deleteItem}
+          createItem={this.props.onCreateItem}
+          startEditing={this.props.onStartEditing}
+          cancelEditing={this.props.onCancelEditing}
+          updateItem={this.props.onUpdateItem}
+          deleteItem = {this.props.onDeleteItem}
           />
       </div>
     );
