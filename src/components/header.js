91599e917ac70c8f2d1461184760470981424026
Avidness <adavidness@gmail.com>
2018-09-01 22:54:05 -0500

    Update header icons. Add placeholders for creating new items and categories

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography }  from '@material-ui/core';
-import FaIcon from '@fortawesome/react-fontawesome';
-import { faMobileAlt } from '@fortawesome/free-solid-svg-icons';
+import { AppBar, Toolbar, Icon, IconButton, Typography }  from '@material-ui/core';
+import { Assignment, CreateNewFolder, LibraryAdd  } from '@material-ui/icons';
 
 import MenuContainer from '../containers/MenuContainer';
-
 import '../static/styles/header.css';
 
 const Header = () => {
   return (
     <AppBar id='header'>
       <Toolbar>
-        <IconButton color="inherit" aria-label="Menu">
-          <FaIcon icon={faMobileAlt} />
-        </IconButton>
+        <Icon color="inherit">
+          <Assignment />
+        </Icon>
         <Typography variant="title" color="inherit">
           Notes
         </Typography>
 
         <MenuContainer />
 
+        <IconButton color="inherit" aria-label="New Category">
+          <CreateNewFolder />
+        </IconButton>
+        <IconButton color="inherit" aria-label="New Item">
+          <LibraryAdd />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
