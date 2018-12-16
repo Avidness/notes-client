77f9c9078f21c3dd5d2450965f0aeb88a084068a
Avidness <adavidness@gmail.com>
2018-12-16 15:09:02 -0600

    Fix import name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import store from './redux/store';
 import HeaderContainer from './containers/HeaderContainer';
 import Footer from './components/Footer';
 import NewCategoryContainer from './containers/NewCategoryContainer';
-import CategoryContainer from './containers/EditCategoryContainer';
+import EditCategoryContainer from './containers/EditCategoryContainer';
 import NewItemContainer from './containers/NewItemContainer';
 import EditItemContainer from './containers/EditItemContainer';
 import ItemListContainer from './containers/ItemListContainer';
@@ -25,7 +25,7 @@ class App extends React.Component {
               <Route path='/item/:itemid' component={EditItemContainer} />
               <Route path='/newitem' component={NewItemContainer} />
               <Route path='/newcategory' component={NewCategoryContainer} />
-                <Route path='/category' component={CategoryContainer} />
+              <Route path='/category' component={EditCategoryContainer} />
               <Redirect to="/items" />
             </Switch>
             <Footer />
