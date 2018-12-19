4854bd31c15f39a0a63a003b9a10585e719390d3
Avidness <adavidness@gmail.com>
2018-12-18 21:49:11 -0600

    Add material ui theme, set it to dark

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import HeaderContainer from './containers/HeaderContainer';
 import Footer from './components/Footer';
@@ -13,24 +14,35 @@ import ItemListContainer from './containers/ItemListContainer';
 
 import './static/styles/App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    secondary: {
+      main: '#5D6D7E',
+    },
+  },
+});
+
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
-          <Fragment>
-            <HeaderContainer />
-            <Switch>
-              <Route path='/items' component={ItemListContainer} />
-              <Route path='/item/:itemid' component={EditItemContainer} />
-              <Route path='/newitem' component={NewItemContainer} />
-              <Route path='/newcategory' component={NewCategoryContainer} />
-              <Route path='/category/:catid' component={EditCategoryContainer} />
-              <Redirect to="/items" />
-            </Switch>
-            <Footer />
-          </Fragment>
-        </BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Fragment>
+              <HeaderContainer />
+              <Switch>
+                <Route path='/items' component={ItemListContainer} />
+                <Route path='/item/:itemid' component={EditItemContainer} />
+                <Route path='/newitem' component={NewItemContainer} />
+                <Route path='/newcategory' component={NewCategoryContainer} />
+                <Route path='/category/:catid' component={EditCategoryContainer} />
+                <Redirect to="/items" />
+              </Switch>
+              <Footer />
+            </Fragment>
+          </BrowserRouter>
+        </MuiThemeProvider>
       </Provider>
     );
   }
