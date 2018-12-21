e4f7075c4037f1c0df5dc2f388532bb81223bd94
Avidness <adavidness@gmail.com>
2018-12-21 14:31:10 -0600

    Move header buttons into a menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,92 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import _ from 'lodash/core';
-import { AppBar, Toolbar, Icon, IconButton, Typography, Tabs, Tab }  from '@material-ui/core';
-import { Assignment, CreateNewFolder, LibraryAdd, Edit, Folder } from '@material-ui/icons';
+import { AppBar, Button, Toolbar, Icon, IconButton, Typography, Tabs, Tab, Menu, MenuItem }  from '@material-ui/core';
+import { Assignment, CreateNewFolder, LibraryAdd, Edit, Folder, Menu as MenuIcon } from '@material-ui/icons';
 
 import '../static/styles/header.css';
 
-const Header = (props) => {
-  return (
-    <AppBar id='header'>
-      <Toolbar>
-        <Icon color="inherit">
-          <Assignment />
-        </Icon>
-        <Typography variant="title" color="inherit">
-          Notes
-        </Typography>
+class Header extends React.Component {
+  state = {
+    anchorEl: null,
+  };
 
-        <Tabs 
-          id='tab-container'
-          onChange={props.handleCategoryChange} 
-          value={props.selectedCategoryId} 
-          scrollable>
+  handleMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
-          {_.map(props.categories, function(cat, key) {
-            return <Tab 
-              key={cat.id}
-              value={cat.id}
-              label={cat.label}
-              icon={<Folder />} />
-          })}
-        </Tabs>
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
-        <Link to={'/category/' + props.selectedCategoryId}>
-          <IconButton color="inherit" aria-label="Edit Category">
-            <Edit />
-          </IconButton>
-        </Link>
+  handleMenuClick = (url) => {
+    this.props.history.push(url);
+    this.handleMenuClose();
+  };
 
-        <Link to={'/newcategory'}>
-          <IconButton color="inherit" aria-label="New Category">
-            <CreateNewFolder />
-          </IconButton>
-        </Link>
+  render(){
+    const { anchorEl } = this.state;
+    return (
+      <AppBar id='header'>
+        <Toolbar>
+          <Icon color="inherit">
+            <Assignment />
+          </Icon>
+          <Typography variant="title" color="inherit">
+            Notes
+          </Typography>
 
-        <Link to={'/newitem'}>
-          <IconButton color="inherit" aria-label="New Item">
-            <LibraryAdd />
-          </IconButton>
-        </Link>
-      </Toolbar>
-    </AppBar>
-  )
+          <Tabs 
+            id='tab-container'
+            onChange={this.props.handleCategoryChange} 
+            value={this.props.selectedCategoryId} 
+            scrollable>
+
+            {_.map(this.props.categories, function(cat, key) {
+              return <Tab 
+                key={cat.id}
+                value={cat.id}
+                label={cat.label}
+                icon={<Folder />} />
+            })}
+          </Tabs>
+
+          <Button color='inherit'
+            aria-owns={anchorEl ? 'simple-menu' : undefined}
+            aria-haspopup="true"
+            onClick={this.handleMenuOpen}>
+            <MenuIcon />
+          </Button>
+
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={this.handleMenuClose}
+            color='inherit'>
+            <MenuItem onClick={() => this.handleMenuClick('/category/' + this.props.selectedCategoryId)}>
+                <IconButton color='inherit' aria-label='Edit Category'>
+                  <Edit /> 
+                </IconButton>
+                Edit Current Category
+            </MenuItem>
+            <MenuItem onClick={() => this.handleMenuClick('/newcategory')}>
+                <IconButton color='inherit' aria-label='New Category'>
+                  <CreateNewFolder />
+                </IconButton> 
+                New Category
+            </MenuItem>
+            <MenuItem onClick={() => this.handleMenuClick('/newitem')}>
+                <IconButton color='inherit' aria-label='New Item'>
+                  <LibraryAdd /> 
+                </IconButton>
+                Add Item
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
-export default Header;
+export default withRouter(Header);
