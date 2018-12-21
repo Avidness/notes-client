296d44574392890f0a5ec796d528d66f3e065cc0
Avidness <adavidness@gmail.com>
2018-12-21 14:31:34 -0600

    Remove unused reference

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
-import { Link } from 'react-router-dom';
 import _ from 'lodash/core';
 import { AppBar, Button, Toolbar, Icon, IconButton, Typography, Tabs, Tab, Menu, MenuItem }  from '@material-ui/core';
 import { Assignment, CreateNewFolder, LibraryAdd, Edit, Folder, Menu as MenuIcon } from '@material-ui/icons';
@@ -65,22 +64,22 @@ class Header extends React.Component {
             onClose={this.handleMenuClose}
             color='inherit'>
             <MenuItem onClick={() => this.handleMenuClick('/category/' + this.props.selectedCategoryId)}>
-                <IconButton color='inherit' aria-label='Edit Category'>
-                  <Edit /> 
-                </IconButton>
-                Edit Current Category
+              <IconButton color='inherit' aria-label='Edit Category'>
+                <Edit /> 
+              </IconButton>
+              Edit Current Category
             </MenuItem>
             <MenuItem onClick={() => this.handleMenuClick('/newcategory')}>
-                <IconButton color='inherit' aria-label='New Category'>
-                  <CreateNewFolder />
-                </IconButton> 
-                New Category
+              <IconButton color='inherit' aria-label='New Category'>
+                <CreateNewFolder />
+              </IconButton> 
+              New Category
             </MenuItem>
             <MenuItem onClick={() => this.handleMenuClick('/newitem')}>
-                <IconButton color='inherit' aria-label='New Item'>
-                  <LibraryAdd /> 
-                </IconButton>
-                Add Item
+              <IconButton color='inherit' aria-label='New Item'>
+                <LibraryAdd /> 
+              </IconButton>
+              Add Item
             </MenuItem>
           </Menu>
         </Toolbar>
